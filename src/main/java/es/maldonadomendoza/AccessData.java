44060ff131aaf96b668065d4d44731911e93241f
reyalfre <alfredorafael1995@hotmail.com>
2021-12-03 02:40:59 +0100

    finalizando trabajo

--- src/main/java/es/maldonadomendoza/AccessData.java
+++ src/main/java/es/maldonadomendoza/AccessData.java
@@ -1,13 +1,18 @@
 package es.maldonadomendoza;
 
+import es.maldonadomendoza.controller.CommitController;
+import es.maldonadomendoza.controller.EquipoController;
 import es.maldonadomendoza.controller.IssueController;
 import es.maldonadomendoza.database.DataBaseController;
+import es.maldonadomendoza.dto.CommitDTO;
+import es.maldonadomendoza.dto.EquipoDTO;
 import es.maldonadomendoza.dto.IssueDTO;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -74,5 +79,50 @@ public class AccessData {
                 .texto("Prueba update")
                 .build();
         System.out.println(issueController);
+    }
+    public void Equipo(){
+        EquipoController equipoController = EquipoController.getInstance();
+        System.out.println("GET Todos los equipos");
+        System.out.println(equipoController.getAllEquiposJSON());
+
+        System.out.println("GET Equipo con ID = 2");
+        System.out.println(equipoController.getEquipoByIdJSON(2));
+
+        System.out.println("UPDATE Equipo con ID = 5");
+        List<String> cambios = new ArrayList<String>();
+        cambios.add("Pablo");
+        EquipoDTO equipoDTO = EquipoDTO.builder()
+                .id(5)
+                .programadores(cambios)
+                .build();
+        System.out.println(equipoController.updateEquipoJSON(equipoDTO));
+        System.out.println("DELETE Equipo con ID = 5");
+        equipoDTO = EquipoDTO.builder()
+                .id(5)
+                .build();
+        System.out.println(equipoController.deleteEquipoJSON(equipoDTO));
+
+    }
+    public void Commit(){
+        CommitController commitController = CommitController.getInstance();
+
+        System.out.println("GET Todos los commits");
+        System.out.println(commitController.getAllCommitsJSON());
+
+        System.out.println("GET Commit con ID = 2");
+        System.out.println(commitController.getCommitByIdJSON(2));
+
+        System.out.println("UPDATE Comite con ID = 5");
+        CommitDTO commitDTO = CommitDTO.builder()
+                .id(5)
+                .titulo("actualizacion")
+                .build();
+        System.out.println(commitController.updateCommitJSON(commitDTO));
+
+        System.out.println("DELETE Commit con ID = 5");
+        commitDTO = CommitDTO.builder()
+                .id(5)
+                .build();
+        System.out.println(commitController.deleteCommitJSON(commitDTO));
     }
 }
